af45d29c1e25f591da94f8435e0640e7f61598c5
shubham <shubhamgorde@gmail.com>
2024-02-13 23:11:19 +0530

    --- Auto Git Commit ---

--- sample.tf
+++ sample.tf
@@ -2,12 +2,38 @@ provider "aws" {
      region = "eu-west-3"
 }
 
+# Create Security Group
+resource "aws_security_group" "my_sg" {
+    name = "my-sec-group"
+    description = "allow-ssh and http"
+    vpc_id = var.vpc_id
+    ingress {
+        protocol = "TCP"
+        from_port = 22
+        to_port  = 22
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        protocol = "TCP"
+        from_port = "80"
+        to_port  = "80"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    egress {
+        protocol = "-1"  
+        from_port = 0
+        to_port = 0
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+
 # Launch AWS instance 
 resource "aws_instance" "my_instance" {
     ami = var.image_id
     instance_type = var.machine_type
     key_name = "shubham"
-    vpc_security_group_ids = ["sg-0bbdb99d529d27057"]
+    vpc_security_group_ids = ["sg-0bbdb99d529d27057", aws_security_group.my_sg.id]
     tags = {
         Name = "my-instance"
         env = "dev"
