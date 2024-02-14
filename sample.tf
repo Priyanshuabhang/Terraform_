da895f4d944fe949f3eb6db7937cae3b0ac18d8a
priyanshu <priyanshu@123>
2024-02-14 21:04:33 +0530

    some

--- sample.tf
+++ sample.tf
@@ -32,8 +32,8 @@ resource "aws_security_group" "my_sg" {
 resource "aws_instance" "my_instance" {
     ami = var.image_id
     instance_type = var.machine_type
-    key_name = "shubham"
-    vpc_security_group_ids = ["sg-0bbdb99d529d27057", aws_security_group.my_sg.id]
+    key_name = "priyanshu"
+    vpc_security_group_ids = ["sg-09ab8dd55ab1c4f0b", aws_security_group.my_sg.id]
     tags = {
         Name = "my-instance"
         env = "dev"
@@ -43,8 +43,8 @@ resource "aws_instance" "my_instance" {
 resource "aws_instance" "another_instance" {
     ami = var.image_id
     instance_type = var.machine_type
-    key_name = "shubham"
-    vpc_security_group_ids = ["sg-0bbdb99d529d27057"]
+    key_name = "priyanshu"
+    vpc_security_group_ids = ["sg-09ab8dd55ab1c4f0b"]
     tags = {
         Name = "another-instance"
         env = "dev"
@@ -54,7 +54,7 @@ resource "aws_instance" "another_instance" {
 # Variables
 
 variable "image_id" {
-    default = "ami-089c89a80285075f7"
+    default = "ami-03f4878755434977f"
 }
 
 variable "machine_type" {
@@ -62,5 +62,5 @@ variable "machine_type" {
 }
 
 variable "vpc_id" {
-    default = "vpc-0e110ef78b813e6d3"
+    default = "vpc-0f0f8d7ace9adb154"
 }
